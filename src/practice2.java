7eb870c73c585c16d20faca7793d72fe211dc3f5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-13 10:53:16 -0700

    practice

---
+++ src/practice2.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class practice2 {
+public static void main(String[] args) {
+	
+	
+	//String Q=JOptionPane.showInputDialog("what is your favorite day of the week?");
+	
+	//if(Q.equalsIgnoreCase("monday")) {
+		//System.out.println("you're lying");
+	//}
+	//else {
+		//System.out.println("nice!");
+	//}
+		
+		
+	//	String w =JOptionPane.showInputDialog("choose a number");
+		//int num= Integer.parseInt(w);
+		//for (int i = 1; i <=10; i++) {
+			
+		//int E= num*i;
+		//System.out.println(E);	
+			
+	
+	
+	//	}
+	
+	//int r=0;
+	
+	//r=new Random().nextInt();
+	
+	//r=new Random().nextInt(133)-37;
+	
+	//System.out.println(r );
+	
+	
+	//String h=JOptionPane.showInputDialog("how many inches of rain have fallen?");
+	
+	//int num=Integer.parseInt(h);
+	
+	//if(num<=2) {
+	//	System.out.println("watch out for fires!");
+	//}
+	//else if(num>2 && num<10) {
+	//	System.out.println("the plants are happy");
+	//}
+	//else {
+	//	System.out.println("you better buy a boat");
+	//}
+		
+	for (int i = 1; i < 100; i++) {
+		if(i%7==0) {
+			System.out.println(i);
+		}
+		
+	}	
+	
+}
+}
+
+
